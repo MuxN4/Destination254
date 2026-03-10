3cf32acd4ad1aca571f3f7e3ad58a4ca1822dda0
MuxN4 <stev3npy@gmail.com>
2026-03-10 10:51:26 +0300

    feat(auth) add JWT token generation and improve registration validations

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,25 +1,36 @@
 import express from "express";
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
+
+const generateToken = (userId) => {
+    return jwt.sign({userId}, process.env.JWT_SECRET, { expiresIn: "15d" });
+}
 
 router.post("/register", async (req, res) => {
     try {
         const { username, email, password } = req.body;
 
-        if(!username || !email || !password) {
+        // check required fields
+        if (!username || !email || !password) {
             return res.status(400).json({ message: "All fields are required" });
         }
 
-        if(password.length < 6) {
-            return res.status(400).json({ message: "Password should be at least 6 characters long" });
+        // validations
+        if (password.length < 6) {
+            return res.status(400).json({
+                message: "Password should be at least 6 characters long"
+            });
         }
 
-        if(username.length < 3) {
-            return res.status(400).json({ message: "Username should be at least 3 characters long" });
+        if (username.length < 3) {
+            return res.status(400).json({
+                message: "Username should be at least 3 characters long"
+            });
         }
 
-        // CHeck if user already exists
+        // check if user already exists
         const existingEmail = await User.findOne({email});
         if(existingEmail) {
             return res.status(400).json({ message: "Email already exists" });
@@ -30,15 +41,33 @@ router.post("/register", async (req, res) => {
             return res.status(400).json({ message: "Username already exists" });
         }
 
+        // get random avatar
+        const profileImage = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`;
+
         const user = new User({
             email,
             username,
             password,
-            profileImage
+            profileImage,
         })
 
+        await user.save();
+
+        const token = generateToken(user._id);
+
+        res.status(201).json({
+            token,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                profileImage: user.profileImage,
+            },
+        });
+
     } catch (error) {
-        
+        console.log("Error in the register route", error);
+        res.status(500).json({ message: "Internal server error" });
     }
 });
 
