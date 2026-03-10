275b8a0475299c7a464fa1eb117989c359521377
MuxN4 <stev3npy@gmail.com>
2026-03-10 11:27:16 +0300

    add password comparison method

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: ""
     }
-}); 
+}, { timestamps: true }); 
 
 // hash password before saving user to db
 userSchema.pre("save", async function(next) {
@@ -31,7 +31,12 @@ userSchema.pre("save", async function(next) {
     this.password = await bcrypt.hash(this.password, salt);
 
     next();
-})
+});
+
+//  compare password
+userSchema.methods.comparePassword = async function (userPassword) {
+    return await bcrypt.compare(userPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
