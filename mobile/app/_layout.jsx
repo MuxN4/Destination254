4865f892aea02ad4296b6268febc95c53c79a731
MuxN4 <stev3npy@gmail.com>
2026-03-17 12:16:29 +0300

    feat(layout) enhance RootLayout with authentication checks and font loading

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,21 +1,74 @@
-import { Stack } from "expo-router";
+import {
+  SplashScreen,
+  Stack,
+  useRouter,
+  useSegments,
+  useRootNavigationState,
+} from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeScreen from "../components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+import { useAuthStore } from "../store/authStore";
+import { useEffect, useState } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const router = useRouter();
+  const segments = useSegments();
+  const navigationState = useRootNavigationState();
+
+  const { checkAuth, user, token } = useAuthStore();
+
+  const [isReady, setIsReady] = useState(false);
+
+  const [fontsLoaded] = useFonts({
+    "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
+  });
+
+  // Load auth + fonts together
+  useEffect(() => {
+    const init = async () => {
+      await checkAuth();
+      setIsReady(true);
+    };
+    init();
+  }, []);
+
+  // Hide splash ONLY when everything is ready
+  useEffect(() => {
+    if (fontsLoaded && isReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, isReady]);
+
+  // Safe navigation
+  useEffect(() => {
+    if (!navigationState?.key || !isReady || !fontsLoaded) return;
+
+    if (!segments.length) return; // extra safety
+
+    const inAuthScreen = segments[0] === "(auth)";
+    const isSignedIn = !!user && !!token;
+
+    if (!isSignedIn && !inAuthScreen) {
+      router.replace("/(auth)");
+    } else if (isSignedIn && inAuthScreen) {
+      router.replace("/(tabs)");
+    }
+  }, [user, token, segments, navigationState, isReady, fontsLoaded]);
+
   return (
     <SafeAreaProvider>
       <SafeScreen>
-
         <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" />
+          <Stack.Screen name="(tabs)" />
           <Stack.Screen name="(auth)" />
         </Stack>
-
       </SafeScreen>
-      
-      <StatusBar style="dark"/>
+
+      <StatusBar style="dark" />
     </SafeAreaProvider>
   );
 }
