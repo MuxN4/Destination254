03cf502d7f5a4e3e2a23eca181e01ccc9e5af46f
MuxN4 <stev3npy@gmail.com>
2026-03-19 17:44:46 +0300

    feat(auth) add isCheckingAuth state to manage authentication loading

--- mobile/store/authStore.js
+++ mobile/store/authStore.js
@@ -6,6 +6,7 @@ export const useAuthStore = create((set) => ({
     user: null,
     token: null,
     isLoading: false,
+    isCheckingAuth: true,
 
     register: async (username, email, password) => {
         set({ isLoading: true });
@@ -37,6 +38,8 @@ export const useAuthStore = create((set) => ({
         } catch (error) {
             set({isLoading: false});
             return { success: false, error: error.message};
+        } finally {
+            set({isCheckingAuth:false})
         }
     },
 
